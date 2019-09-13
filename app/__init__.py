853ba9cf78188f26782c682e45aa0fe9f878d3b0
Rog <roger.murray@iis.se>
2019-09-13 17:43:39 +0200

    made progress on parsing first section of dig response

--- app/__init__.py
+++ app/__init__.py
@@ -34,9 +34,11 @@ example_response = {
 
 class DiG(Resource):
     def get(self):
-        result = subprocess.run(["dig", "iis.se"], stdout=subprocess.PIPE)
+        result = subprocess.run(["dig", "internetstiftelsen.se"], stdout=subprocess.PIPE)
         answer = result.stdout.decode().split(sep="\n\n")
-        return self.parse_basic_info(answer[0])
+        basic_info = self.parse_basic_info(answer[0])
+        return basic_info
+        # return self.parse_basic_info(answer[0])
         self.parse_opt_section(answer[1])
         self.parse_answer_section(answer[2])
         self.parse_summary_info(answer[3])
@@ -51,9 +53,10 @@ class DiG(Resource):
 
         for row in info.split(sep="\n"):
             if "DiG" in row:
+                print(row)
                 part = {
-                    "text": "",  # TODO add text with placeholders
-                    "desc": "the version dig and query sent",
+                    "text": "<<>> DiG $0 <<>> $1",  # TODO add text with placeholders
+                    "desc": "The version of dig and the query sent",
                     "parts": [],
                 }
                 print(row)
@@ -71,6 +74,120 @@ class DiG(Resource):
                         {
                             "text": match_object.group(2),
                             "desc": "This is the query that was sent.",
+                        }
+                    )
+                    basic_info["parts"].append(part)
+            elif "global options" in row:
+                part = {
+                    "text": "Global Options",  # TODO add text with placeholders
+                    "desc": "The global options set.",
+                    "parts": [],
+                }
+                print(row)
+                match_object = re.match(
+                    re.compile(r";; global options: (\+\w+)\s*"), row
+                    # TODO check if there are other global options and handle them
+                )
+                if match_object:
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(1),
+                            "desc": "global options that were set.",
+                        }
+                    )
+                    basic_info["parts"].append(part)
+            elif "Got answer" in row:
+                part = {
+                    "text": "Got answer",  # TODO add text with placeholders
+                    "desc": "Is this every anything else??.",
+                    "parts": [],
+                }
+                print(row)
+                match_object = re.match(
+                    re.compile(r";; Got answer:\s*(.*)\s*"), row
+                    # TODO check if there are other global options and handle them
+                )
+                if match_object:
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(1),
+                            "desc": "No idea what this is actually.",
+                        }
+                    )
+                    basic_info["parts"].append(part)
+            elif "HEADER" in row:
+                part = {
+                    "text": "->>HEADER<<- opcode: $0, status: $1, id: $2",  # TODO add text with placeholders
+                    "desc": "Header row including opcode, response code and message id.",
+                    "parts": [],
+                }
+                print(row)
+                match_object = re.match(
+                    re.compile(r";; ->>HEADER<<- opcode: (\w*), status: (\w*), id: (\d*)"), row
+                )
+                if match_object:
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(1),
+                            "desc": "Opcode Query.", # TODO research if Opcode is always a query
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(2),
+                            "desc": "Status Code.",
+                            # TODO write code to handle descriptions for the
+                            #  possible status codes
+                            #  RCODE https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(3),
+                            "desc": "The message id.",
+                        }
+                    )
+                    basic_info["parts"].append(part)
+            elif "flags" in row:
+                part = {
+                    "text": ";; flags: $0; QUERY: $1, ANSWER: $2, AUTHORITY: $3, ADDITIONAL: $4",  # TODO add text with placeholders
+                    "desc": "Flags and section count.",
+                    "parts": [],
+                }
+                print(row)
+                match_object = re.match(
+                    re.compile(r";; flags: (\w.*); QUERY: (\d*), ANSWER: (\d*), AUTHORITY: (\d*), ADDITIONAL: (\d*)"), row
+                )
+                if match_object:
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(1),
+                            "desc": "DNS Header Flags", # TODO parse flags and create descriptions
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(2),
+                            "desc": "Number of queries",
+                            # TODO check if this is ever more than 1
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(3),
+                            "desc": "Number in the ANSWER section",
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(4),
+                            "desc": "Number in the AUTHORITY section",
+                        }
+                    )
+                    part["parts"].append(
+                        {
+                            "text": match_object.group(5),
+                            "desc": "Number in the ADDITIONAL section",
                         }
                     )
                     basic_info["parts"].append(part)
