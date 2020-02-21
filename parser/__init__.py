191ab5da0bfd153dd1045dc87dd69e0ff4237d4b
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 17:19:55 +0100

    Doc

--- parser/__init__.py
+++ parser/__init__.py
@@ -7,6 +7,9 @@ def parse(dig_output: str) -> dict:
 
     parsed_output["basic_info"] = basic_info.parse(answer[0])
 
+    # The Opt and Question sections are both on the same text segment in dig.
+    # Find where the question section begins and split the string there for parsing the sections.
+    # Index will be 0 if the is no Opt section.
     question_index = answer[1].find(";; QUESTION SECTION:")
     parsed_output["question_section"] = question_section.parse(answer[1][question_index:])
 
