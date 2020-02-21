3c2d2b7bf43f54b19fa01387a0f201df97ed322c
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 16:57:00 +0100

    Parse data class

--- parser/question_section.py
+++ parser/question_section.py
@@ -13,6 +13,8 @@ def parse(question: str):
     parts = question.splitlines()[1].split()
 
     section["parts"].append(_domain_name(parts[0]))
+    section["parts"].append(_class(parts[1]))
+
     return section
 
 
@@ -22,5 +24,24 @@ def _domain_name(value: str):
         "text": value,
         "desc": "This is the domain name that is queried."
     }
+    return part
+
+
+def _class(value: str):
+    part = {
+        "text": value.upper(),
+        "desc": f"This is the data class that is queried: {value.upper()}\n"
+    }
+
+    classes = {
+        "IN": "IN stands for Internet, it is the default class used for most DNS queries.",
+        "CH": "CH stands for Chaos, it is commonly used for things like querying DNS server versions.",
+    }
+
+    try:
+        part["desc"] += classes[value.upper()]
+    except KeyError:
+        part["desc"] += "Could not find class description."
+
     return part
 
