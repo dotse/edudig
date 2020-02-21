0751119ca27bf21f4a5b9108d2ec56da374e20e5
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 16:50:53 +0100

    Parse domain name

--- parser/question_section.py
+++ parser/question_section.py
@@ -12,4 +12,15 @@ def parse(question: str):
     # Split on newline and get second line, then go through every part (word) in that line
     parts = question.splitlines()[1].split()
 
+    section["parts"].append(_domain_name(parts[0]))
     return section
+
+
+def _domain_name(value: str):
+    value = value.strip(";")
+    part = {
+        "text": value,
+        "desc": "This is the domain name that is queried."
+    }
+    return part
+
