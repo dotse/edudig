2143c36400662468c1af17c2a6f73550054af712
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 16:48:54 +0100

    Add basic parsing for question section

---
+++ parser/question_section.py
@@ -0,0 +1,12 @@
+import re
+
+
+def parse(question: str):
+    section = {
+        "text": ";; QUESTION SECTION:\n"
+                ";$0\t$1\t$2\n",
+        "desc": "The question section contains bla bla",
+        "parts": []
+    }
+
+    return section
