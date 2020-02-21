f54b72592d3146e6929487290ca5e5d50a282e89
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 17:17:11 +0100

    parse question section

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,9 +1,13 @@
-from parser import basic_info
+from parser import basic_info, question_section
 
 
 def parse(dig_output: str) -> dict:
     answer = dig_output.split(sep="\n\n")
     parsed_output = {}
+
     parsed_output["basic_info"] = basic_info.parse(answer[0])
+
+    question_index = answer[1].find(";; QUESTION SECTION:")
+    parsed_output["question_section"] = question_section.parse(answer[1][question_index:])
 
     return parsed_output
