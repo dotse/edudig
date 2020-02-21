126f318795622504c7506068d84a436dd90a05f5
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-02-21 14:14:24 +0100

    Add variable for parsed output from different sections

--- parser/__init__.py
+++ parser/__init__.py
@@ -4,3 +4,7 @@ from parser import basic_info
 def parse(dig_output: str) -> dict:
     answer = dig_output.split(sep="\n\n")
     return basic_info.parse(answer[0])
+    parsed_output = {}
+    parsed_output["basic_info"] = basic_info.parse(answer[0])
+
+    return parsed_output
