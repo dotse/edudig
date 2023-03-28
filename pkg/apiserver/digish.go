b7dc74210524c33b65a4ba0b62d346d5a90d7a05
romu42 <romu42@gmail.com>
2023-03-28 13:11:52 +0200

    cleaned type definitions that I moved to client.go

--- pkg/apiserver/digish.go
+++ pkg/apiserver/digish.go
@@ -12,41 +12,6 @@ import (
 	"github.com/miekg/dns"
 )
 
-/*
-// Query contains the information needed to create a DNS query
-type Query struct {
-	Zone       string `json:"Zone"`
-	Nameserver string `json:"Nameserver"`
-	Transport  string `json:"Transport"`
-	Qname      string `json:"Qname"`
-	Qtype      string `json:"Qtype"`
-	Port       string `json:"Port"`
-	Recursion  string `json:"Recursion"`
-}
-
-// Response is used to rename the fields dns.Msg.Ns -> Response.Authority
-// and dns.Msg.Extra -> Response.Additional to make it easier for the front end.
-type Response struct {
-	MsgHdr     dns.MsgHdr
-	Compress   bool
-	Question   []dns.Question
-	Answer     []dns.RR
-	Authority  []dns.RR
-	Additional []dns.RR
-}
-
-// Dug contains the reply from the dns query and some extra data to be returned to the frontend
-type Dug struct {
-	Zone       string        `json:"Zone"`
-	Resp       Response      `json:"Response"`
-	RTT        time.Duration `json:"Round trip time"`
-	Nameserver string        `json:"Nameserver"`
-	MsgSize    int           `json:"Message Size"`
-	Transport  string        `json:"Transport"`
-}
-
-*/
-
 // digish gets query data from the frontend, creates and sends a dns query.
 // The reply is parsed and sent back to the frontend.
 func digish(w http.ResponseWriter, r *http.Request) {
